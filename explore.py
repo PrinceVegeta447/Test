e5df96adec8045984a3c3c95d500fb6971883c9f
PrinceVegeta447 <vegetakun447@gmail.com>
2024-12-24 12:56:30 +0530

    Update explore.py

--- explore.py
+++ explore.py
@@ -19,23 +19,3 @@ def explore(update: Update, context: CallbackContext):
         update.message.reply_text(f"You encountered {enemy_encountered}! Prepare to fight!")
     else:
         update.message.reply_text("You explored the area but found nothing. Keep searching!")
-
-# Add the handler to the dispatcher
-def main():
-    from telegram.ext import Updater
-    
-    # Replace 'YOUR_BOT_TOKEN' with your actual bot token
-    updater = Updater("YOUR_BOT_TOKEN")
-    
-    # Get the dispatcher to register the command
-    dispatcher = updater.dispatcher
-    
-    # Register the explore command
-    dispatcher.add_handler(CommandHandler("explore", explore))
-    
-    # Start the bot
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
