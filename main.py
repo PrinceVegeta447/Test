8054aaab95df56f4aeeb4979b67b1d3e4d875381
PrinceVegeta447 <vegetakun447@gmail.com>
2024-12-24 15:33:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from play import play, character_callback
 from view import viewch
 from explore import explore, inventory
 from inventory import add_item, get_inventory
-from battle import battle, attack, defend
+from battle import battle, handle_battle_action
 import logging
 
 # Load Config
@@ -35,9 +35,8 @@ def main():
     application.add_handler(CallbackQueryHandler(character_callback))
     application.add_handler(CommandHandler("explore", explore))
     application.add_handler(CommandHandler("inventory", inventory))
-    application.add_handler(CallbackQueryHandler(battle, pattern=r"^battle_"))
-    application.add_handler(CallbackQueryHandler(attack, pattern=r"^attack_"))
-    application.add_handler(CallbackQueryHandler(defend, pattern=r"^defend_"))
+    application.add_handler(CallbackQueryHandler(battle, pattern=r"^battle_.*$"))  # Trigger battle when enemy encountered
+    application.add_handler(CallbackQueryHandler(handle_battle_action, pattern=r"^(attack|defend|run)$"))  # Battle action handlers
     
     # Start polling
     application.run_polling()
